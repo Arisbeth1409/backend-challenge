8598b86d77f60cc909ee464833fe131efe5d98dc
Arisbeth Martínez <aremi.martinez@gmail.com>
2024-08-17 13:49:29 -0600

    feat(server): cors and helmet

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const usersRoutes = require("./routes/users.routes");
 const postsRoutes = require("./routes/posts.routes");
 
 const app = express();
+
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", usersRoutes);
